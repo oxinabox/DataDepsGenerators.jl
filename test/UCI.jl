8dfb4793759c53a573226bce767199089f6c2900
Lyndon White <oxinabox@ucc.asn.au>
2018-01-13 23:44:10 +0800

    =Generalise Generators

--- test/UCI.jl
+++ test/UCI.jl
@@ -4,11 +4,9 @@ using Base.Test
 using ReferenceTests
 
 @test_reference "references/UCI Air+Quality.txt" generate(UCI(), "Air+Quality")
-@test_reference "references/UCI auto mpg.txt" generate(UCI(), "auto+mpg")
+@test_reference "references/UCI auto mpg.txt" generate(UCI(), "https://archive.ics.uci.edu/ml/datasets/auto+mpg")
 @test_reference "references/UCI banking marketting.txt" generate(UCI(), "bank+marketing")
-@test_reference "references/UCI BHP.txt" generate(UCI(), "Burst+Header+Packet+%28BHP%29+flooding+attack+on+Optical+Burst+Switching+%28OBS%29+Network")
-# ^ With names like that, the user is really going to want to edit the automatically determined short name into something shorted
-
+@test_reference "references/UCI BHP.txt" generate(UCI(), "Burst+Header+Packet+%28BHP%29+flooding+attack+on+Optical+Burst+Switching+%28OBS%29+Network", "Burst Header Packet (UCI)")
 
 @test_reference "references/UCI Adult.txt" generate(UCI(), "Adult")
 

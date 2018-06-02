2bd2fb74cacfccae2a4815b0fd5881e263811bf8
Sebastin Santy <sebastinssanty@gmail.com>
2018-06-02 06:08:56 +0100

    only TERN

--- test/DataOnev2/KNB.jl
+++
@@ -1,9 +0,0 @@
-using DataDepsGenerators
-using Base.Test
-
-using ReferenceTests
-
-@testset "KNB test" begin
-    @test_reference "../references/KNB/ArcticDataCenter Influenza A.txt" generate(ArcticDataCenter(), "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/doi:10.5063%2FF1HT2M7Q")
-    @test_reference "../references/KNB/KnowledgeNetworkforBiocomplexity Forage Fish.txt" generate(KnowledgeNetworkforBiocomplexity(), "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/doi:10.5063/F1T43R7N")
-end

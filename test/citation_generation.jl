2176242a326a2cc1a43d03ed73323045eda89c5d
Lyndon White <oxinabox@ucc.asn.au>
2018-06-27 19:27:54 +0800

    =add tests

---
+++ test/citation_generation.jl
@@ -0,0 +1,10 @@
+using DataDepsGenerators
+using Base.Test
+
+@test citation_text("10.3732/ajb.1000481") ==
+    "Smith, S. A., Beaulieu, J. M., Stamatakis, A., & Donoghue, M. J. (2011). Understanding angiosperm diversification using small and large phylogenetic trees. American Journal of Botany, 98(3), 404–414. doi:10.3732/ajb.1000481"
+
+
+
+@test citation_text("https://doi.org/10.5061/dryad.8790") ==
+    "Smith, S. A., Beaulieu, J. M., Stamatakis, A., & Donoghue, M. J. (2011). Data from: Understanding angiosperm diversification using small and large phylogenetic trees [Data set]. Dryad Digital Repository. https://doi.org/10.5061/dryad.8790"

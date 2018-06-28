de452c2960dc06c9c2559ac0a60e04dfde1c6199
Lyndon White <oxinabox@ucc.asn.au>
2018-06-28 13:39:39 +0800

    =don

--- src/utils.jl
+++ src/utils.jl
@@ -27,20 +27,14 @@ Indents each line in a string
 indent(str, indentwith="\t") = join(indentwith.*strip.(split(str, "\n")), "\n")
 
 
-Base.escape_string(s::AbstractString, esc::AbstractString) = sprint(endof(s), escape_string, s, esc*"\"")
-
 """
     escape_multiline_string
 
 like Escape string, but does not escape newlines
 """
-function escape_multiline_string(s::AbstractString, esc::AbstractString)
-    escaped = escape_string(s, esc)
-    # Newlines and quotes do not need escaping in multiline strings
-    escaped = replace(escaped, "\\n", "\n")
-    escaped = replace(escaped, "\\r", "\r")
-    escaped = replace(escaped, "\\\"", "\"")
-    # tabs never need to be escaped in strings
-    escaped = replace(escaped, "\\t", "\t")
+function escape_multiline_string(s::AbstractString)
+    escaped = s
+    escaped = replace(escaped, '\$', raw"\$")
+    escaped = replace(escaped, '\\', raw"\\")
     escaped
 end

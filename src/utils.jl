c2e4d88f649ade9211bcf4ef20e9d6555d13fffd
Lyndon White <oxinabox@ucc.asn.au>
2018-06-28 12:49:49 +0800

    =better escaping rules

--- src/utils.jl
+++ src/utils.jl
@@ -36,5 +36,11 @@ like Escape string, but does not escape newlines
 """
 function escape_multiline_string(s::AbstractString, esc::AbstractString)
     escaped = escape_string(s, esc)
-    replace(escaped, "\\n", "\n")
+    # Newlines and quotes do not need escaping in multiline strings
+    escaped = replace(escaped, "\\n", "\n")
+    escaped = replace(escaped, "\\r", "\r")
+    escaped = replace(escaped, "\\\"", "\"")
+    # tabs never need to be escaped in strings
+    escaped = replace(escaped, "\\t", "\t")
+    escaped
 end

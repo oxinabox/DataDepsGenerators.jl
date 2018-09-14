8185b41d75bbaaea535510e5c95fbe1006daefcf
Lyndon White <oxinabox@ucc.asn.au>
2018-09-14 16:06:21 +0800

    =update Project.toml

--- src/DataDepsGenerators.jl
+++ src/DataDepsGenerators.jl
@@ -3,7 +3,7 @@ using Gumbo, Cascadia, AbstractTrees
 using Suppressor
 using JSON
 using HTTP
-using Date
+using Dates
 
 export generate, citation_text, remove_cite_version
 export UCI, GitHub, DataDryad, DataOneV1, DataOneV2, CKAN, DataCite, Figshare, JSONLD
